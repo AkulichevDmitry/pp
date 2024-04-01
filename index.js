09bf479a6aef06ed97c0e30b17a19b35e0697747
AkulichevDmitry <118440313+AkulichevDmitry@users.noreply.github.com>
2024-04-01 19:34:03 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -45,3 +45,23 @@ result_button.addEventListener("click", () => {
     result_field.innerHTML = v.countStrength() + p.countStrength() + r.countStrength()
     console.log(v)
 })
+
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
+}
+
+function onload() {
+    auto_grow(document.getElementById("result"));
+}
+
+function generate() {
+    let subject = document.getElementById("subject").value;
+    let event = document.getElementById("event").value;
+    let event2 = document.getElementById("event2").value;
+    if (subject.length==0 || event.length==0 || event2.length==0)
+        return;
+    let result = `　　${subject}${event}是怎么回事呢？${subject}相信大家都很熟悉，但是${subject}${event}是怎么回事呢，下面就让小编带大家一起了解吧。\r\n　　${subject}${event}，其实就是${event2}，大家可能会很惊讶${subject}怎么会${event}呢？但事实就是这样，小编也感到非常惊讶。\r\n　　这就是关于${subject}${event}的事情了，大家有什么想法呢，欢迎在评论区告诉小编一起讨论哦！`;
+    document.getElementById("result").value = result;
+    auto_grow(document.getElementById("result"));
+}
